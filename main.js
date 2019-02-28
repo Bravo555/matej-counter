428a2579e5e29aa621684e2d2ed80a9a3789ff06
Marcel Guzik <mguzik555@gmail.com>
2019-02-28 10:15:06 +0100

    fuck it

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const MATEJS_START = 32;
 
-app.use(express.static('public'));
+let today = new Date();
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+function weeks_elapsed_since(date1, date2) {
+    let oneday = 24 * 60 * 60 * 1000;
+    let dayDiff = Math.round(Math.abs((date2.getTime() - date1.getTime()) / (oneday)));
+
+    return Math.floor(dayDiff / 7);
+}
+
+function matejs_passed(today) {
+    let thursday = new Date(2019, 1, 21);
+    let monday = new Date(2019, 1, 25);
+
+    let thursdays_passed = weeks_elapsed_since(thursday, today)
+    let mondays_passed = weeks_elapsed_since(monday, today)
+
+    return MATEJS_START - (mondays_passed + 3 * thursdays_passed);
+}
+
+document.getElementById('xd').innerHTML = matejs_passed(today);
