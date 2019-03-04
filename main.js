307a7bd0bb39b932f5fecdfec2ec18402816ece8
Marcel Guzik <mguzik555@gmail.com>
2019-03-04 10:44:48 +0100

    PROPER TIME COUNTING

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 const MATEJS_START = 32;
 
-let today = new Date();
-
 function weeks_elapsed_since(date1, date2) {
     let oneday = 24 * 60 * 60 * 1000;
-    let dayDiff = Math.round(Math.abs((date2.getTime() - date1.getTime()) / (oneday)));
+    let timeDiff = Math.abs((date2.getTime() - date1.getTime()) / (oneday));
+    let dayDiff = Math.floor(timeDiff);
 
     return Math.floor(dayDiff / 7);
 }
 
 function matejs_passed(today) {
-    let thursday = new Date(2019, 1, 21);
-    let monday = new Date(2019, 1, 25);
+    let lessons = [
+        new Date(2019, 1, 25, 12, 40),
+        new Date(2019, 1, 21, 8, 15),
+        new Date(2019, 1, 21, 10, 55),
+        new Date(2019, 1, 21, 11, 50)
+    ];
 
-    let thursdays_passed = weeks_elapsed_since(thursday, today)
-    let mondays_passed = weeks_elapsed_since(monday, today)
-
-    return MATEJS_START - (mondays_passed + 3 * thursdays_passed);
+    let hours_passed = lessons.reduce((hours, lesson) => hours += weeks_elapsed_since(lesson, today), 0);
+    return hours_passed;
 }
 
-document.getElementById('xd').innerHTML = matejs_passed(today);
+let today = new Date();
+document.getElementById('xd').innerHTML = (MATEJS_START - matejs_passed(today));
